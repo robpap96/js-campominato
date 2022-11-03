741e7f050675192e4f20c5b6ed33f5dbeb4e7091
robpap96 <robertopapagni1996@outlook.it>
2022-11-03 19:22:34 +0100

    quasi finito

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // Consegna
-// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
 // In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora
 // di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 // La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti
@@ -8,36 +7,36 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 "use strict";
 
+//creo array bombe
+const bombe = [];
 function crea16Bombe(str, range){
     for(let i=0; i<16; i++) {
-        let num = Math.round(Math.random()*range + 1);
-        if (i > 0 && !str.includes(num)) {
-            str += ", ";
-        } 
+        let num = parseInt(Math.random()*range + 1);
         if(str.includes(num)){
            num = Math.round(Math.random()*range + 1); 
            i--;
            continue;
         }
-        str += num;
+        str.push(num);
     }
     //per deb
-    //console.log( "Numeri generati: " + str);
-
+    console.log( "Numeri generati: " + str +", ");
 }
 
 const boardContainer = document.querySelector(".board");
 const playButton = document.getElementById("btn-play");
-//creo array bombe
-const bombe = "";
 let clicked = false;
 
 playButton.addEventListener("click", function(){
+
     let levelSelected = document.getElementById("level-selected").value;
     //FACILE
     if(clicked === false && levelSelected === "facile") {
 
+        let punteggio = 0;
         boardContainer.classList.remove("display__none");
+        let cellClick = false;
+        crea16Bombe(bombe, 100);
         for(let i=1; i<=100; i++){
             //creo elemento
             const boardCell = document.createElement("div");
@@ -45,15 +44,26 @@ playButton.addEventListener("click", function(){
             boardCell.classList.add("board__number");
             //aggiungo evento click
             boardCell.addEventListener("click", function() {
-                console.log(this.innerHTML);
-                this.classList.add("blue__cell");
-            });  
+                let num = parseInt(this.innerHTML);
+                if(bombe.includes(num) && cellClick === false){
+                    this.classList.add("red__cell");
+                    cellClick = true;
+                    //prova stampa messaggio
+                    const message = document.querySelector(".w-or-l");
+                    message.innerHTML = '<h1 class="text-align-center"> HAI PERSO !!! </h1>';
+                } else if(cellClick === false){
+                    this.classList.add("blue__cell");
+                    //deb
+                }
+                
+            }); 
+            //punteggio++;
+            //console.log(`il tuo punteggio è di -> ${punteggio}`); 
             //concateno l'elemento
             boardContainer.append(boardCell);
         }
         clicked = true; 
-        //CREO LE BOMBE
-        crea16Bombe(bombe, 100);
+        
     } 
         //MEDIO
         else if(clicked === false && levelSelected === "medio") {
@@ -63,6 +73,8 @@ playButton.addEventListener("click", function(){
             const boardCell = document.createElement("div");
             boardCell.innerHTML = i;
             boardCell.classList.add("board__number");
+            //CREO LE BOMBE
+            crea16Bombe(bombe, 81);
             //aggiungo evento click
             boardCell.addEventListener("click", function() {
                 console.log(this.innerHTML);
@@ -72,8 +84,7 @@ playButton.addEventListener("click", function(){
             boardContainer.append(boardCell);
         }
         clicked = true;
-        //CREO LE BOMBE
-        crea16Bombe(bombe, 81);
+
     } 
         //DIFFICILE
         else if (clicked === false && levelSelected === "difficile") {
@@ -83,6 +94,8 @@ playButton.addEventListener("click", function(){
             const boardCell = document.createElement("div");
             boardCell.innerHTML = i;
             boardCell.classList.add("board__number");
+            //CREO LE BOMBE
+            crea16Bombe(bombe, 49);
             //aggiungo evento click
             boardCell.addEventListener("click", function() {
                 console.log(this.innerHTML);
@@ -92,8 +105,6 @@ playButton.addEventListener("click", function(){
             boardContainer.append(boardCell);
         }
         clicked = true;
-        //CREO LE BOMBE
-        crea16Bombe(bombe, 49);
 
     } else {
         alert("Hai gia creato la board!!");
